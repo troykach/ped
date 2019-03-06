bb35335a63fa5d684fee0e4b3cc27d3586305f31
troykach <36416733+troykach@users.noreply.github.com>
2019-03-06 10:36:39 +0300

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            double k = Convert.ToDouble(input[0]);
+            double x = Convert.ToDouble(input[1]);
+            int n;
+            double sum = 1;
+            double obrfact = 1;
+            for (n = 1; n <= k; n++)
+            {
+                obrfact /= n;
+                sum += (Math.Cos(n * x) * obrfact);
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
